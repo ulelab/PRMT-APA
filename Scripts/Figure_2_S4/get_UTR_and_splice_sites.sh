6d7c2ec9f3ea5eb3ecc58a3ed3e8d56ab6798f66
llywelyng <60936369+llywelyng@users.noreply.github.com>
2025-09-16 14:17:33 +0100

    Update get_UTR_and_splice_sites.sh

--- Scripts/Figure_2_S4/get_UTR_and_splice_sites.sh
+++ Scripts/Figure_2_S4/get_UTR_and_splice_sites.sh
@@ -1,6 +1,6 @@
 # Description:
 # This script uses the GTFtools package to generate bed files containing locations of splice sites and UTR regions
 
-python GTFtools/gtftools.py -q splice_regions.bed gencode.v45.annotation.gtf
+python GTFtools/gtftools.py -q ../../Data/Figure_1_S1_S2_S3/classification_of_APA_bed_files/splice_regions.bed /path/to/gencode.v45.annotation.gtf
 
-python GTFtools/gtftools.py -u UTR_regions_gencode_v45.bed gencode.v45.annotation.gtf
+python GTFtools/gtftools.py -u ../../Data/Figure_1_S1_S2_S3/classification_of_APA_bed_files/UTR_regions_gencode_v45.bed /path/to/gencode.v45.annotation.gtf
