46017ffb256b2075f257e9e041611164e1e43d0b
llywelyng <60936369+llywelyng@users.noreply.github.com>
2025-09-16 17:02:17 +0100

    Update get_fasta_files.sh

--- Scripts/Figure_3_S5_S6/get_fasta_files.sh
+++ Scripts/Figure_3_S5_S6/get_fasta_files.sh
@@ -3,14 +3,14 @@
 #### this script generates fasta files from input bed files
 
 # Iterate over directories within the "bed" directory
-for dir in /ulelab/PRMT-APA/Scripts/scripts_for_figures_in_paper/bed_files/200nt_window/DMAi_cancer_line_panel/*; do
+for dir in /ulelab/PRMT-APA/Data/Figure_3_S5_S6/bed/APA_3seq/1000nt_window/*; do
     # Extract the directory name
     dir_name=$(basename "$dir")
     
     # Iterate over "*.bed" files within the directory
     for bedfile in "$dir"/*.bed; do
         filename=$(basename "$bedfile" .bed)
-        outputfile="fasta/200nt_window/DMAi_cancer_line_panel/$dir_name/${filename}.fasta" #example directory, change to where you want to save fasta files 
+        outputfile="/ulelab/PRMT-APA/Data/Figure_3_S5_S6/fasta/1000nt_window/$dir_name/${filename}.fasta" 
         bedtools getfasta -fi /path/to/genome_fasta_file/GRCh38.primary_assembly.genome.fa -bed "$bedfile" -fo "$outputfile" -s -name
     done
 done
