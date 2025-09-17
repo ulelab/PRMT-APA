f358222d64c07ccbee7d20254b3ce0d82e47fe57
llywelyng <60936369+llywelyng@users.noreply.github.com>
2025-09-17 16:18:05 +0100

    Update get_fasta_files.sh

--- Scripts/Figure_4_S7/get_fasta_files.sh
+++ Scripts/Figure_4_S7/get_fasta_files.sh
@@ -2,15 +2,19 @@
 
 #### this script generates fasta files from input bed files
 
-# Iterate over directories within the "bed" directory
-for dir in /ulelab/PRMT-APA/Scripts/scripts_for_figures_in_paper/bed_files/200nt_window/DMAi_cancer_line_panel/*; do
-    # Extract the directory name
-    dir_name=$(basename "$dir")
+input_dir="ulelab/PRMT-APA/Data/Figure_4_S7/Figure_4_S7/bed/APA_3seq/1000nt_window"  # change to boxplot_regions/ or /200nt_window to generate those respective fasta files
+outdir_dir="ulelab/PRMT-APA/Data/Figure_4_S7/Figure_4_S7/fasta/APA_3seq/1000nt_window"
+
+# Iterate over "*.bed" files within the input directory
+for bedfile in "$input_dir"/*.bed; do
+    # Extract the base filename without extension
+    filename=$(basename "$bedfile" .bed)
     
-    # Iterate over "*.bed" files within the directory
-    for bedfile in "$dir"/*.bed; do
-        filename=$(basename "$bedfile" .bed)
-        outputfile="fasta/200nt_window/DMAi_cancer_line_panel/$dir_name/${filename}.fasta" #example directory, change to where you want to save fasta files 
-        bedtools getfasta -fi /path/to/genome_fasta_file/GRCh38.primary_assembly.genome.fa -bed "$bedfile" -fo "$outputfile" -s -name
-    done
+    # Define the output file path
+    outputfile="$output_dir/${filename}.fasta"
+    
+   # Run bedtools to convert bed to fasta
+    bedtools getfasta -fi /path/to/GRCh38.primary_assembly.genome.fa -bed "$bedfile" -fo "$outputfile" -s -name
 done
+
+
